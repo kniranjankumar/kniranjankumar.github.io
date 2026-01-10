7ecebe2a4c53a7f584a93f4a9441ca3a0396541b
Niranjan Kumar <niranjankumar@gatech.edu>
2026-01-10 14:28:00 -0800

    update

--- bday2026/plan.js
+++ bday2026/plan.js
@@ -7,7 +7,7 @@ const planData = [
         prep: [
             "Departure: 3:00 PM from Santa Clara.",
             "Pack your knitting/crochet and a good book for the ride.",
-            "Pack and dress for a literary trip — we have dinner reservations."
+            "Dress for a literary evening — we have dinner reservations."
         ]
     },
     {
@@ -16,25 +16,32 @@ const planData = [
         destination: "Hotel Emblem San Francisco",
         prep: [
             "Relinquish your heavy burdens at the desk.",
-            "Seek out the Writer's Alcove in the heart of the hall."
-        ]
+            "Tonight is about the stories we create."]
     },
     {
         time: "2026-01-10T19:00:00-08:00",
-        title: "Chapter III: A Toast to Great Characters",
+        title: "Chapter III: We might be old, but the night is young",
         destination: "Novela",
         prep: [
-            "Bring only your spirit and a curiosity for storied concoctions.",
-            "Choose a drink named for a hero or a rogue."
-        ]
+            "We don't need a designated driver.",]
     },
     {
         time: "2026-01-11T07:00:00-08:00",
         title: "Chapter IV: The Morning Prologue",
         destination: "Réveille Coffee Co.",
         prep: [
-            "Don your most comfortable traveling shoes.",
-            "Prepare a satchel for the treasures (books and fibers) we shall find."
+            "As always, coffee first."
+        ]
+    },
+    {
+        time: "2026-01-11T08:00:00-08:00",
+        title: "Chapter V: Write your own story",
+        destination: "San Francisco City",
+        prep: [
+            "City Lights Booksellers & Publishers",
+            "ImagiKnit",
+            "Museum of Craft and Design",
+            "Tartine Manufactory"
         ]
     }
 ];
